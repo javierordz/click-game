da91b3fde51ef3521d9e8550b718f72827d25361
javierordz <javier.ordz@gmail.com>
2019-06-26 21:42:49 -0500

    Updates

---
+++ precache-manifest.9512ae6cb37e9435c9db9f4fcc42f32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "c23135b0f33c5ff0f79a",
+    "url": "/click-game/static/js/main.2023daed.chunk.js"
+  },
+  {
+    "revision": "9b2473ce90804090947e",
+    "url": "/click-game/static/js/2.aaef2e63.chunk.js"
+  },
+  {
+    "revision": "c23135b0f33c5ff0f79a",
+    "url": "/click-game/static/css/main.90e48f67.chunk.css"
+  },
+  {
+    "revision": "96f8002fe69e5e43893197138fa3cadd",
+    "url": "/click-game/index.html"
+  }
+];
